20cd6aa723af74816c64b4c3ae6ed3ab88c83b56
unknown <siddharthpal819@gmail.com>
2025-04-09 18:27:21 -0400

    update return message in app

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Pal ECS Container."
+    return "This is Assignment 3 for Deploying Cloud Infrastructure class - Hello from Pal ECS Container."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
